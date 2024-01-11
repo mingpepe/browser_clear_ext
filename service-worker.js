f3a53a0b8f423dcfd0336caaa6ced268a48f14d2
mingpepe <mingpepemath@gmail.com>
2024-01-11 20:17:54 +0800

    feat: create new tab and remove all other tabs

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,12 @@
-chrome.commands.onCommand.addListener(function (command) {
+chrome.commands.onCommand.addListener(async function (command) {
     if (command === "clearBrowserData") {
+        const newTab = await chrome.tabs.create({});
+        const tabs = await chrome.tabs.query({});
+        for (let i = 0; i < tabs.length; i++) {
+            if (tabs[i].id !== newTab.id) {
+                chrome.tabs.remove(tabs[i].id);
+            }
+        }
         clearBrowserData();
     }
 });
